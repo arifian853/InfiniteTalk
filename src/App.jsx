e38fd8cdb6d761f75d90d1985e712b71911b70f4
Arifian <arifiansaputra43@gmail.com>
2023-11-04 20:39:32 +0700

    Added authentication system (5%) - still error

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Route, Routes } from 'react-router-dom'
 
 import { LandingPage } from './Pages/LandingPage'
@@ -19,6 +19,7 @@ import 'aos/dist/aos.css'
 import { Feed } from './Pages/Feed';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState('')
   useEffect(() => {
     AOS.init();
   })
@@ -27,14 +28,20 @@ function App() {
     <>
       <Routes>
         <Route path='/' exact element={<LandingPage />} />
+
         <Route path='*' element={<NotFound />}/>
-        <Route path='/signin' element={<SignIn />} />
+
+        <Route path='/signin' element={<SignIn setIsAuthenticated={setIsAuthenticated}/>} />
+
         <Route path='/signup' element={<SignUp />} />
+
         <Route path='/mentors' element={<MentorPage />} />
+
         <Route path='/mentors/mentors-signin' element={<MentorSignIn />} />
+
         <Route path='/mentors/mentors-signup' element={<MentorSignUp />} />
 
-        <Route path='/feed' element={<Feed />} />
+        <Route path='/feed' element={<Feed isAuthenticated={isAuthenticated} />} />
       </Routes>
     </>
   )
--- src/Pages/Mentee_Role/SignIn.jsx
+++ src/Pages/Mentee_Role/SignIn.jsx
@@ -1,36 +1,133 @@
-import { Button, Checkbox } from 'flowbite-react';
-import { Helmet } from 'react-helmet';
-import { Link, useNavigate } from 'react-router-dom';
+/* eslint-disable react/prop-types */
+import { useState } from 'react'
+import { Alert, Button, Checkbox } from 'flowbite-react'
+import { Helmet } from 'react-helmet'
+import { Link, Navigate } from 'react-router-dom'
+import axios from 'axios'
 
-export const SignIn = () => {
-    const navigate = useNavigate()
-    const goBack = () => {
-        navigate(-1)
+import { HiInformationCircle } from 'react-icons/hi'
+
+export const SignIn = ({ setIsAuthenticated }) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [navigate, setNavigate] = useState(false)
+    const [error, setError] = useState('')
+    const [alert, setAlert] = useState('')
+
+    const onChangeUsername = (e) => {
+        const value = e.target.value;
+        setUsername(value);
+        setError('')
     }
+
+    const onChangePassword = (e) => {
+        const value = e.target.value;
+        setPassword(value);
+        setError('')
+    }
+
+    const loginBtn = () => {
+        const data = {
+            username: username,
+            password: password
+        }
+
+        axios.post('http://localhost:7777/user/login', data)
+            .then(result => {
+                console.log(result)
+                if (result) {
+                    setIsAuthenticated(true)
+                    setAlert('Login successful!')
+                    setTimeout(() => {
+                        setNavigate(true)
+                    }, 2000)
+                } else {
+                    setError('Login failed. Please check your credentials.');
+                }
+            })
+            .catch(e => {
+                setError(e.response.data.message)
+            })
+    }
+
+    const backButton = () => {
+        window.history.back();
+    };
+
     return (
-        <div className="forms flex flex-col justify-center items-center gap-4">
-            <h1 className='text-3xl font-semibold'>Sign in to <Link to='/'><span className="text-blue-400"> Infinite</span><span className="text-green-400">Talk!</span></Link></h1>
-            <p>Welcome back, Mentee!</p>
-            <Helmet>
-                <title>InfiniteTalk! - Sign In</title>
-            </Helmet>
-            <div data-aos="zoom-in" className="form-light text-sm flex flex-col text-left items-left gap-2 p-3">
-                <p>Username</p>
-                <input type="text" />
-                <p>Password</p>
-                <input type="password" name="" id="" />
-                <div className="flex items-center gap-2 py-1">
-                    <Checkbox id="remember" />
-                    <p>Remember me</p>
-                </div>
-                <Button>
-                    Sign In
-                </Button>
-                <div className='text-sm text-center'>
-                    <p> Doesn&apos;t have an account? <Link to='/signup'> <span className='underline'>Make one</span> </Link></p>
-                    <span onClick={goBack} className='underline cursor-pointer'>Cancel</span>
+
+        <>
+            {
+                navigate && (
+                    <Navigate to="/feed" />
+                )
+            }
+            <div className="forms flex flex-col justify-center items-center gap-4">
+                <h1 className='text-3xl font-semibold'>Sign in to <Link to='/'><span className="text-blue-400"> Infinite</span><span className="text-green-400">Talk!</span></Link></h1>
+                <p>Welcome back, Mentee!</p>
+                <Helmet>
+                    <title>InfiniteTalk! - Sign In</title>
+                </Helmet>
+                <div data-aos="zoom-in" className="form-light text-sm flex flex-col text-left items-left gap-2 p-3">
+                    {
+                        error && (
+                            <Alert
+                                color="failure"
+                                icon={HiInformationCircle}
+                            >
+                                <span>
+                                    <p>
+                                        <span className="font-medium">
+                                            {error}
+                                        </span>
+
+                                    </p>
+                                </span>
+                            </Alert>
+                        )
+                    }
+                    {
+                        alert && (
+                            <Alert
+                                color="success"
+                                icon={HiInformationCircle}
+                            >
+                                <span>
+                                    <p>
+                                        <span className="font-medium">
+                                            {alert}
+                                        </span>
+
+                                    </p>
+                                </span>
+                            </Alert>
+                        )
+                    }
+                    <p>Username</p>
+                    <input type="text" id="username1"
+                        placeholder="Your username"
+                        required
+                        value={username}
+                        onChange={onChangeUsername} />
+                    <p>Password</p>
+                    <input type="password" id="password1"
+                        required
+                        value={password}
+                        onChange={onChangePassword} />
+                    <div className="flex items-center gap-2 py-1">
+                        <Checkbox id="remember" />
+                        <p>Remember me</p>
+                    </div>
+                    <Button onClick={loginBtn}>
+                        Sign In
+                    </Button>
+                    <div className='text-sm text-center'>
+                        <p> Doesn&apos;t have an account? <Link to='/signup'> <span className='underline'>Make one</span> </Link></p>
+                        <span onClick={backButton} className='underline cursor-pointer'>Cancel</span>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
+
     )
 }
