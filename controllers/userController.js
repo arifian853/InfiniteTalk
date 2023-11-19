32864613a33f6dbed905491f39a6e64b113cc51b
Arifian <arifiansaputra43@gmail.com>
2023-11-19 14:52:25 +0700

    Added TOTP function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,24 @@
 import User from '../models/userData.model.js';
 import bcryptjs from 'bcryptjs';
+import crypto from 'crypto';
+import * as OTPAuth from 'otpauth';
+import pkg from 'hi-base32';
+const { encode } = pkg;
+
+const generateRandomBase32 = () => {
+    const buffer = crypto.randomBytes(15);
+    const base32 = encode(buffer).replace(/=/g, '').substring(0, 24);
+    return base32;
+};
+
+const generateTOTP = (base32Secret) => {
+    return new OTPAuth.TOTP({
+        issuer: 'YOUR_ISSUER_NAME',
+        algorithm: 'SHA1',
+        digits: 6,
+        secret: base32Secret,
+    });
+};
 
 const UserRegistration = async (req, res, next) => {
     try {
@@ -10,7 +29,7 @@ const UserRegistration = async (req, res, next) => {
         if (user) {
             return res.status(404).json({
                 status: false,
-                message: 'Email already used. Please use another email.'
+                message: 'Email already used. Please use another email.',
             });
         }
 
@@ -18,7 +37,7 @@ const UserRegistration = async (req, res, next) => {
         if (usernameUser) {
             return res.status(404).json({
                 status: false,
-                message: 'Username already used. Please choose another username.'
+                message: 'Username already used. Please choose another username.',
             });
         }
 
@@ -32,8 +51,20 @@ const UserRegistration = async (req, res, next) => {
             password: hashPassword, // Hashed password
             program,
             mentor,
-            admin
+            admin,
         });
+
+        // TOTP related code
+        const base32Secret = generateRandomBase32();
+        const totp = generateTOTP(base32Secret);
+
+        const otpAuthURL = totp.toString();
+
+        user.otp_auth_url = otpAuthURL;
+        user.otp_base32 = base32Secret;
+
+        // Save the user with TOTP-related information
+        await user.save();
 
         return res.status(201).json({
             _id: user._id,
@@ -44,6 +75,87 @@ const UserRegistration = async (req, res, next) => {
             admin: user.admin,
             mentor: user.mentor,
             token: await user.generateJWT(),
+            otp_auth_url: otpAuthURL, // Include this in the response
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const GenerateOTP = async (req, res, next) => {
+    try {
+        const { user_id } = req.body;
+
+        const user = await User.findById(user_id);
+
+        if (!user) {
+            return res.status(404).json({
+                status: 'fail',
+                message: 'No user with that ID exists',
+            });
+        }
+
+        const base32Secret = generateRandomBase32();
+        const totp = generateTOTP(base32Secret);
+
+        const otpAuthURL = totp.toString();
+
+        user.otp_auth_url = otpAuthURL;
+        user.otp_base32 = base32Secret;
+
+        // Save the updated TOTP-related information
+        await user.save();
+
+        res.status(200).json({
+            base32: base32Secret,
+            otp_auth_url: otpAuthURL,
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const VerifyOTP = async (req, res, next) => {
+    try {
+        const { user_id, token } = req.body;
+
+        const user = await User.findById(user_id);
+
+        if (!user) {
+            return res.status(401).json({
+                status: 'fail',
+                message: 'Token is invalid or user does not exist',
+            });
+        }
+
+        const totp = generateTOTP(user.otp_base32);
+        const delta = totp.validate({ token });
+
+        if (delta === null) {
+            return res.status(401).json({
+                status: 'fail',
+                message: 'Token is invalid or user does not exist',
+            });
+        }
+
+        // Update user data (if needed)
+        user.otp_enabled = true;
+        user.otp_verified = true;
+
+        await user.save();
+
+        res.status(200).json({
+            otp_verified: true,
+            user: {
+                _id: user._id,
+                username: user.username,
+                avatar: user.avatar,
+                fullName: user.fullName,
+                email: user.email,
+                admin: user.admin,
+                mentor: user.mentor,
+                program: user.program,
+            },
         });
     } catch (error) {
         next(error);
@@ -71,6 +183,7 @@ const UserLogin = async (req, res, next) => {
                 mentor: user.mentor,
                 program: user.program,
                 token: await user.generateJWT(),
+                otp_auth_url: user.otp_auth_url, // Include TOTP URL in the response
             });
         } else {
             throw new Error("Invalid email or password");
@@ -93,7 +206,10 @@ const UserProfile = async (req, res, next) => {
                 email: user.email,
                 admin: user.admin,
                 mentor: user.mentor,
-                program: user.program
+                program: user.program,
+                otp_enabled: user.otp_enabled,
+                otp_verified: user.otp_verified,
+                otp_auth_url: user.otp_auth_url, // Include TOTP URL in the response
             });
         } else {
             let error = new Error("User not found");
@@ -103,7 +219,7 @@ const UserProfile = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-}
+};
 
 const UpdateUserProfile = async (req, res, next) => {
     try {
@@ -117,12 +233,11 @@ const UpdateUserProfile = async (req, res, next) => {
         user.email = req.body.email || user.email;
 
         if (req.body.password && req.body.password.length < 8) {
-            throw new Error("Password length must be at least 6 character");
+            throw new Error("Password length must be at least 6 characters");
         } else if (req.body.password) {
             const hashedPassword = await bcryptjs.hash(req.body.password, 10); // Hash the updated password
             user.password = hashedPassword; // Set the hashed password
-        }
-        else {
+        } else {
             user.password = user.password;
         }
 
@@ -134,10 +249,114 @@ const UpdateUserProfile = async (req, res, next) => {
             fullName: newUserProfile.fullName,
             email: newUserProfile.email,
             token: await newUserProfile.generateJWT(),
+            otp_enabled: newUserProfile.otp_enabled,
+            otp_verified: newUserProfile.otp_verified,
+            otp_auth_url: newUserProfile.otp_auth_url, // Include TOTP URL in the response
         });
     } catch (error) {
         next(error);
     }
 };
 
-export { UserRegistration, UserLogin, UserProfile, UpdateUserProfile };
+const UpdateProfilePicture = async (req, res, next) => {
+    try {
+        const upload = uploadPicture.single("profilePicture");
+
+        upload(req, res, async function (err) {
+            if (err) {
+                const error = new Error(
+                    "An unknown error occured when uploading " + err.message
+                );
+                next(error);
+            } else {
+                if (req.file) {
+                    let filename;
+                    let newUserProfile = await User.findById(req.user._id);
+                    filename = newUserProfile.avatar;
+                    if (filename) {
+                        fileRemover(filename);
+                    }
+                    newUserProfile.avatar = req.file.filename;
+                    await newUserProfile.save();
+                    res.json({
+                        _id: newUserProfile._id,
+                        avatar: newUserProfile.avatar,
+                        fullName: newUserProfile.fullName,
+                        email: newUserProfile.email,
+                        admin: updatedUser.admin,
+                        mentor: updatedUser.mentor,
+                        token: await newUserProfile.generateJWT(),
+                    });
+                } else {
+                    let filename;
+                    let newUserProfile = await User.findById(req.user._id);
+                    filename = newUserProfile.avatar;
+                    newUserProfile.avatar = "";
+                    await newUserProfile.save();
+                    fileRemover(filename);
+                    res.json({
+                        _id: newUserProfile._id,
+                        avatar: newUserProfile.avatar,
+                        fullName: newUserProfile.fullName,
+                        email: newUserProfile.email,
+                        admin: updatedUser.admin,
+                        mentor: updatedUser.mentor,
+                        token: await newUserProfile.generateJWT(),
+                    });
+                }
+            }
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+const DisableOTP = async (req, res, next) => {
+    try {
+        const { user_id } = req.body;
+
+        const user = await User.findById(user_id);
+        if (!user) {
+            return res.status(401).json({
+                status: 'fail',
+                message: 'User does not exist',
+            });
+        }
+
+        const updatedUser = await User.findByIdAndUpdate(
+            user_id,
+            { $set: { otp_enabled: false } },
+            { new: true }
+        );
+
+        res.status(200).json({
+            otp_disabled: true,
+            user: {
+                _id: updatedUser._id,
+                username: updatedUser.username,
+                avatar: updatedUser.avatar,
+                fullName: updatedUser.fullName,
+                email: updatedUser.email,
+                admin: updatedUser.admin,
+                mentor: updatedUser.mentor,
+                program: updatedUser.program,
+                otp_enabled: updatedUser.otp_enabled,
+                otp_verified: updatedUser.otp_verified,
+                otp_auth_url: updatedUser.otp_auth_url, // Include TOTP URL in the response
+            },
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export {
+    UserRegistration,
+    UserLogin,
+    UserProfile,
+    UpdateUserProfile,
+    UpdateProfilePicture,
+    GenerateOTP,
+    VerifyOTP,
+    DisableOTP,
+};
--- models/userData.model.js
+++ models/userData.model.js
@@ -1,59 +1,77 @@
 import { Schema, model } from "mongoose";
 import jwt from "jsonwebtoken";
 
-const userDataSchema = new Schema({
+const userDataSchema = new Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        sparse: true
+      type: String,
+      required: true,
+      unique: true,
+      sparse: true,
     },
     password: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     avatar: {
-        type: String,
-        default: ""
+      type: String,
+      default: "",
     },
     fullName: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     program: {
-        type: String
+      type: String,
     },
     mentor: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     admin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
     verificationCode: {
-        type: String,
-        required: false
+      type: String,
+      required: false,
     },
     lastLogin: {
-        type: Date,
-        default: null
+      type: Date,
+      default: null,
     },
-},
-    {
-        timestamps: true
-    });
+    otp_enabled: {
+      type: Boolean,
+      default: false,
+    },
+    otp_verified: {
+      type: Boolean,
+      default: false,
+    },
+    otp_base32: {
+      type: String,
+      default: "",
+    },
+    otp_auth_url: {
+      type: String,
+      default: "",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 userDataSchema.methods.generateJWT = async function () {
-    return await jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-        expiresIn: "30d",
-    });
+  return await jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
 };
 
-const User = model('User', userDataSchema);
+const User = model("User", userDataSchema);
 export default User;
