e7d5769db5042f25ef1314501a39605d91c4dc73
Arifian <arifiansaputra43@gmail.com>
2023-12-13 22:07:17 +0700

    Fix minor bugs

--- middleware/uploadPictureMiddleware.js
+++ middleware/uploadPictureMiddleware.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 const uploadPicture = multer({
   storage: storage,
   limits: {
-    fileSize: 1 * 1000000, // 1MB
+    fileSize: 1 * 10000000, // 10MB
   },
   fileFilter: function (req, file, cb) {
     let ext = path.extname(file.originalname);
